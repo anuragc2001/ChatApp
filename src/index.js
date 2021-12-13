871697dc2824467b71f78a65fe578f60729a5955
Anurag Chakraborty <chakraborty.anurag01@gmail.com>
2021-12-13 17:02:54 +0530

    basic interface setup

--- src/index.js
+++ src/index.js
@@ -1,27 +1,40 @@
-const express = require('express');
 const path = require('path')
 const http = require('http')
-const socketio = require('socket.io');
-const port = process.env.PORT || 3000
+const express = require('express')
+const socketio = require('socket.io')
+// const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
-const app = new express()
+const app = express()
 const server = http.createServer(app)
-
 const io = socketio(server)
 
-const publicDirectoryPath = path.join(__dirname, "../public")
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
-    console.log("New connection established :)")
-    socket.emit('onWelcome', "Welcome")
-    socket.on('newMessage', (message) => {
-        io.emit('onWelcome', message)
+    console.log('New WebSocket connection')
+
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        io.emit('message', generateMessage(message))
+        callback()
     })
 
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!'))
+    })
 })
 
 server.listen(port, () => {
-    console.log("Server started at port ", port)
+    console.log(`Server is up on port ${port}!`)
 })
